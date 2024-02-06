2b301187707e055e0c3fabcd1aa76ef1b8425331
MorgynKass <morgynkass@gmail.com>
2024-02-06 08:20:44 -0700

    first commit

---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("Insert Noun: ")
+plural_noun = input("Insert Plural Noun: ")
+verb = input("Insert Verb: ")
+
+print(f"The big {noun} {verb} around the city in a fury causing damage to all {plural_noun}")
